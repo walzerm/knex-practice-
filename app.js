3f6ec4a50643c61fbb45fc631c63a0ec28fef0b5
Lissa Walzer <lissawalzer@gmail.com>
2015-12-18 14:29:54 -0800

    Adding a form for editing

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ var app = express();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var knex = require('./db/knex');
+require('locus');
+var methodOverride = require('method-override')
 
 //middleware
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('tiny'));
+app.use(methodOverride('_method'));
 
 app.get('/students', function(req, res) {
     knex('students').then(function(students) {
@@ -20,13 +23,25 @@ app.get('/students/new', function(req, res) {
 })
 
 app.post('/students', function(req, res) {
-    console.log(req.body);
     knex('students').insert({name: req.body.new}).then(function() {
         res.redirect('/students');
     })
-
 })
 
+app.get('/students/:id/edit', function(req, res) {
+    //find a student
+    var id = req.params.id;
+    knex('students').where({id: id}).first().then(function(student) {
+        //render edit.ejs and pass into it the student found
+        //eval(locus);
+        res.render('edit', {student: student});
+    })
+})
+
+app.put('students/:id', function() {
+    //use knex to update student and redirect to /students
+    
+})
 
 app.listen(3000, function() {
     console.log('But in the end, it didnt even maaaaaaaaaater...');
